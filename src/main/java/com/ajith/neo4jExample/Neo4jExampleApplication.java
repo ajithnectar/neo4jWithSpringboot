4564d9520cb5d3e6b02d011e546ed8d71abe0ca7
ajithnectarit <ajith@nectarit.com>
2024-07-30 12:51:44 +0530

    project basic setup done

---
+++ src/main/java/com/ajith/neo4jExample/Neo4jExampleApplication.java
@@ -0,0 +1,14 @@
+package com.ajith.neo4jExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jExampleApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/test/java/com/ajith/neo4jExample/Neo4jExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajith.neo4jExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
